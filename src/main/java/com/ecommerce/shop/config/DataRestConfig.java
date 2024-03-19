db2d18c14b27cdadd5367f5f1c205398edc7a7fc
mohammed <alsudanimohammed1@gmail.com>
2024-03-19 15:15:58 +0200

    rest setup

---
+++ src/main/java/com/ecommerce/shop/config/DataRestConfig.java
@@ -0,0 +1,31 @@
+package com.ecommerce.shop.config;
+
+import com.ecommerce.shop.entity.Product;
+import com.ecommerce.shop.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+        HttpMethod[] theUnSupportedActions = {HttpMethod.POST,HttpMethod.PUT, HttpMethod.DELETE};
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnSupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnSupportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnSupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnSupportedActions));
+
+    }
+
+}
